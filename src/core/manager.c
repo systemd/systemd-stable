5ecec44b34f269657ff354b6cf4fbb1c481c0f55
Lennart Poettering <lennart@poettering.net>
2013-04-25 21:57:41 -0300

    unit: rework stop pending logic
    When a trigger unit wants to know if a stop is queued for it, we should
    just check precisely that and do not check whether it is actually
    stopped already. This is because we use these checks usually from state
    change calls where the state variables are not updated yet.
    
    This change splits unit_pending_inactive() into two calls
    unit_inactive_or_pending() and unit_stop_pending(). The former checks
    state and pending jobs, the latter only pending jobs.
    (cherry picked from commit 31afa0a44c2d7f93d837c840cdbd623982ac165f)

--- src/core/manager.c
+++ src/core/manager.c
@@ -2337,7 +2337,7 @@ void manager_reset_failed(Manager *m) {
                 unit_reset_failed(u);
 }
 
-bool manager_unit_pending_inactive(Manager *m, const char *name) {
+bool manager_unit_inactive_or_pending(Manager *m, const char *name) {
         Unit *u;
 
         assert(m);
@@ -2348,7 +2348,7 @@ bool manager_unit_pending_inactive(Manager *m, const char *name) {
         if (!u)
                 return true;
 
-        return unit_pending_inactive(u);
+        return unit_inactive_or_pending(u);
 }
 
 void manager_check_finished(Manager *m) {
