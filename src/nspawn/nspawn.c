98963dfd60e4ae11cd4537e38cb6976da95abc9e
Ken Werner <ken@linux.vnet.ibm.com>
2014-12-16 18:06:41 +0100

    nspawn: fix invocation of the raw clone() system call on s390 and cris
    Since the order of the first and second arguments of the raw clone() system
    call is reversed on s390 and cris it needs to be invoked differently.
    
    (cherry picked from commit 60e1651a31c9c0ed1caef1a63f5e3a87156b0b1e)
    
    Conflicts:
    	src/shared/missing.h

--- src/nspawn/nspawn.c
+++ src/nspawn/nspawn.c
@@ -3187,9 +3187,9 @@ int main(int argc, char *argv[]) {
                         goto finish;
                 }
 
-                pid = syscall(__NR_clone, SIGCHLD|CLONE_NEWNS|
-                                          (arg_share_system ? 0 : CLONE_NEWIPC|CLONE_NEWPID|CLONE_NEWUTS)|
-                                          (arg_private_network ? CLONE_NEWNET : 0), NULL);
+                pid = raw_clone(SIGCHLD|CLONE_NEWNS|
+                                (arg_share_system ? 0 : CLONE_NEWIPC|CLONE_NEWPID|CLONE_NEWUTS)|
+                                (arg_private_network ? CLONE_NEWNET : 0), NULL);
                 if (pid < 0) {
                         if (errno == EINVAL)
                                 log_error("clone() failed, do you have namespace support enabled in your kernel? (You need UTS, IPC, PID and NET namespacing built in): %m");
