1935564a3e2955904d1bd0fec5b3c4de50631a6d
Mike Gilbert <floppymaster@gmail.com>
2017-01-10 02:39:05 -0500

    build-sys: add check for gperf lookup function signature (#5055)
    gperf-3.1 generates lookup functions that take a size_t length
    parameter instead of unsigned int. Test for this at configure time.
    
    Fixes: https://github.com/systemd/systemd/issues/5039
    (cherry picked from commit c9f7b4d356a453a01aa77a6bb74ca7ef49732c08)

--- src/core/load-fragment.h
+++ src/core/load-fragment.h
@@ -113,7 +113,7 @@ int config_parse_fdname(const char *unit, const char *filename, unsigned line, c
 int config_parse_sec_fix_0(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 
 /* gperf prototypes */
-const struct ConfigPerfItem* load_fragment_gperf_lookup(const char *key, unsigned length);
+const struct ConfigPerfItem* load_fragment_gperf_lookup(const char *key, GPERF_LEN_TYPE length);
 extern const char load_fragment_gperf_nulstr[];
 
 typedef enum Disabled {
