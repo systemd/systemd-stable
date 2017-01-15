7770e9fc69d7545b4ebf77567cce3f8b85c7ccf7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-01-15 11:51:48 -0500

    cryptsetup: fix unitialized variable
    CID #1368416.
    
    Coverity web interface is back, yay!
    
    (cherry picked from commit 6fefc0eded00974e5b2862bd5efc7ef975c6a318)

--- src/cryptsetup/cryptsetup.c
+++ src/cryptsetup/cryptsetup.c
@@ -752,6 +752,7 @@ int main(int argc, char *argv[]) {
 
         } else {
                 log_error("Unknown verb %s.", argv[1]);
+                r = -EINVAL;
                 goto finish;
         }
 
