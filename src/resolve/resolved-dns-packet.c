1e20ca63e06337b95f4b0deedc062511d2ff31cc
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-10-25 11:19:19 +0200

    resolved: fix loop on packets with pseudo dns types
    Reported by Karim Hossen & Thomas Imbert from Sogeti ESEC R&D.
    
    https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1725351
    (cherry picked from commit 9f939335a07085aa9a9663efd1dca06ef6405d62)
    (cherry picked from commit 743b771c559c6101544f7358a42c8c519fe4b0db)

--- src/resolve/resolved-dns-packet.c
+++ src/resolve/resolved-dns-packet.c
@@ -1503,7 +1503,7 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
 
                 found = true;
 
-                while (bitmask) {
+                for (; bitmask; bit++, bitmask >>= 1)
                         if (bitmap[i] & bitmask) {
                                 uint16_t n;
 
@@ -1517,10 +1517,6 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
                                 if (r < 0)
                                         return r;
                         }
-
-                        bit++;
-                        bitmask >>= 1;
-                }
         }
 
         if (!found)
