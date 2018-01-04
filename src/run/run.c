4ae164f3f240a7791f561d19b043aad3b450d52c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-01-04 11:12:57 +0100

    run: double free
    Introduced in f2b9f2c83e5b4035b22eb2f610531fce275bf55f. CID #1384218.
    
    (cherry picked from commit 1893972894c197c1ac5999f8f555749bb673c262)

--- src/run/run.c
+++ src/run/run.c
@@ -113,7 +113,7 @@ static void help(void) {
 }
 
 static int add_timer_property(const char *name, const char *val) {
-        _cleanup_free_ char *p = NULL;
+        char *p;
 
         assert(name);
         assert(val);
@@ -124,8 +124,6 @@ static int add_timer_property(const char *name, const char *val) {
 
         if (strv_consume(&arg_timer_property, p) < 0)
                 return log_oom();
-
-        p = NULL;
 
         return 0;
 }
