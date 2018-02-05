e15098196dd8b933a45c23eb2f11461765f45a65
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-02-05 09:54:57 +0100

    gcrypt-util: fix memleak
    (cherry picked from commit bd944e6e1854df9aa6002fed015226d6298029b6)
    (cherry picked from commit 36efac0d4a78df53148d68f222c87781bdebcc76)

--- src/shared/gcrypt-util.h
+++ src/shared/gcrypt-util.h
@@ -19,6 +19,8 @@
   along with systemd; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
+#pragma once
+
 #include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -26,8 +28,12 @@
 #ifdef HAVE_GCRYPT
 #include <gcrypt.h>
 
+#include "macro.h"
+
 void initialize_libgcrypt(bool secmem);
 int string_hashsum(const char *s, size_t len, int md_algorithm, char **out);
+
+DEFINE_TRIVIAL_CLEANUP_FUNC(gcry_md_hd_t, gcry_md_close);
 #endif
 
 static inline int string_hashsum_sha224(const char *s, size_t len, char **out) {
