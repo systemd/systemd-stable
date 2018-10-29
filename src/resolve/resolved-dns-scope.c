systemd/systemd

resolve: set IP_RECVERR
Closes #10345.

src/resolve/resolved-dns-scope.c
@@ -411,6 +411,13 @@ static int dns_scope_socket(
                 }
         }
 
+        if (type == SOCK_DGRAM) {
+                /* Set IP_RECVERR or IPV6_RECVERR to get ICMP error feedback. See discussion in #10345. */
+                r = setsockopt(fd, SOL_IP, sa.sa.sa_family == AF_INET ? IP_RECVERR : IPV6_RECVERR, &const_int_one, sizeof(const_int_one));
+                if (r < 0)
+                        return -errno;
+        }
+
         if (ret_socket_address)
                 *ret_socket_address = sa;
         else {

systemd/systemd-stable

(cherry picked from commit 9fcdab9c8747ab092ac120b58de2ee7b1be9d7db)

src/resolve/resolved-dns-scope.c
@@ -412,6 +412,13 @@ static int dns_scope_socket(
                 }
         }
 
+        if (type == SOCK_DGRAM) {
+                /* Set IP_RECVERR or IPV6_RECVERR to get ICMP error feedback. See discussion in #10345. */
+                r = setsockopt(fd, SOL_IP, sa.sa.sa_family == AF_INET ? IP_RECVERR : IPV6_RECVERR, &one, sizeof(one));
+                if (r < 0)
+                        return -errno;
+        }
+
         if (ret_socket_address)
                 *ret_socket_address = sa;
         else {