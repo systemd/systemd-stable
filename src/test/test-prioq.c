systemd/systemd

basic/prioq: add prioq_peek_item()

src/test/test-prioq.c
@@ -69,6 +69,11 @@ static void test_struct(void) {
         assert_se(q = prioq_new((compare_func_t) test_compare));
         assert_se(s = set_new(&test_hash_ops));
 
+        assert_se(prioq_peek(q) == NULL);
+        assert_se(prioq_peek_by_index(q, 0) == NULL);
+        assert_se(prioq_peek_by_index(q, 1) == NULL);
+        assert_se(prioq_peek_by_index(q, (unsigned) -1) == NULL);
+
         for (i = 0; i < SET_SIZE; i++) {
                 assert_se(t = new0(struct test, 1));
                 t->value = (unsigned) rand();
@@ -78,6 +83,17 @@ static void test_struct(void) {
                 if (i % 4 == 0)
                         assert_se(set_consume(s, t) >= 0);
         }
+
+        for (i = 0; i < SET_SIZE; i++)
+                assert_se(prioq_peek_by_index(q, i));
+        assert_se(prioq_peek_by_index(q, SET_SIZE) == NULL);
+
+        unsigned count = 0;
+        PRIOQ_FOREACH_ITEM(q, t) {
+                assert_se(t);
+                count++;
+        }
+        assert_se(count == SET_SIZE);
 
         while ((t = set_steal_first(s))) {
                 assert_se(prioq_remove(q, t, &t->idx) == 1);

systemd/systemd-stable

(cherry picked from commit ef21b3b5bf824e652addf850bcfd9374c7b33ce8)
(cherry picked from commit 5ae9eda582a81a2a3e53c5525e995848d1ac16db)

src/test/test-prioq.c
@@ -96,6 +96,11 @@ static void test_struct(void) {
         s = set_new(&test_hash_ops);
         assert_se(s);
 
+        assert_se(prioq_peek(q) == NULL);
+        assert_se(prioq_peek_by_index(q, 0) == NULL);
+        assert_se(prioq_peek_by_index(q, 1) == NULL);
+        assert_se(prioq_peek_by_index(q, (unsigned) -1) == NULL);
+
         for (i = 0; i < SET_SIZE; i++) {
                 struct test *t;
 
@@ -111,6 +116,18 @@ static void test_struct(void) {
                         assert_se(r >= 0);
                 }
         }
+
+        for (i = 0; i < SET_SIZE; i++)
+                assert_se(prioq_peek_by_index(q, i));
+        assert_se(prioq_peek_by_index(q, SET_SIZE) == NULL);
+
+        unsigned count = 0;
+        void *tt;
+        PRIOQ_FOREACH_ITEM(q, tt) {
+                assert_se(tt);
+                count++;
+        }
+        assert_se(count == SET_SIZE);
 
         for (;;) {
                 struct test *t;