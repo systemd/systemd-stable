ca937b49da3333692607074399aa0189ca5cb3e6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-07-08 14:18:46 +0200

    pid1: fix serialization/deserialization of commmands with spaces
    Fixes #12258.
    
    This is enough to reproduce:
    $ systemd-run bash -c 'sleep 10' && systemctl daemon-reload
    would result in
    Current command vanished from the unit file.
    
    We would serialize as:
    ExecStart 0 /usr/bin/bash /usr/bin/bash -c sleep 10000
    which of course can't work.
    Now we serialize as
    ExecStart 0 /usr/bin/bash "/usr/bin/bash" "-c" "sleep 10".
    
    (cherry picked from commit 334c0979f37adab7cf6df01cff1ee818bf72d7b2)

--- src/core/service.c
+++ src/core/service.c
@@ -2507,14 +2507,16 @@ static int service_serialize_exec_command(Unit *u, FILE *f, ExecCommand *command
                         return log_oom();
 
                 n = strlen(e);
-                if (!GREEDY_REALLOC(args, allocated, length + 1 + n + 1))
+                if (!GREEDY_REALLOC(args, allocated, length + 2 + n + 2))
                         return log_oom();
 
                 if (length > 0)
                         args[length++] = ' ';
 
+                args[length++] = '"';
                 memcpy(args + length, e, n);
                 length += n;
+                args[length++] = '"';
         }
 
         if (!GREEDY_REALLOC(args, allocated, length + 1))
@@ -2659,7 +2661,7 @@ static int service_deserialize_exec_command(Unit *u, const char *key, const char
         for (;;) {
                 _cleanup_free_ char *arg = NULL;
 
-                r = extract_first_word(&value, &arg, NULL, EXTRACT_CUNESCAPE);
+                r = extract_first_word(&value, &arg, NULL, EXTRACT_CUNESCAPE | EXTRACT_QUOTES);
                 if (r < 0)
                         return r;
                 if (r == 0)
