f384c8b45b3efabadcd141a4f0dc1e3a67cef027
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-01-15 13:38:30 +0100

    sd-device: silence gcc warning with newest gcc
    (cherry picked from commit 376ee2c312b87951028a0adff96b1052f32475fa)
    (cherry picked from commit 18aff8c85720606e05826045b6799d19a7dcf08a)
    (cherry picked from commit e05023045edd4a0e20b60e81f9fa54f08636d660)

--- src/libsystemd/sd-device/sd-device.c
+++ src/libsystemd/sd-device/sd-device.c
@@ -1347,7 +1347,7 @@ int device_read_db_internal_filename(sd_device *device, const char *filename) {
         _cleanup_free_ char *db = NULL;
         const char *value;
         size_t db_len, i;
-        char key;
+        char key = '\0';  /* Unnecessary initialization to appease gcc-12.0.0-0.4.fc36 */
         int r;
 
         enum {
