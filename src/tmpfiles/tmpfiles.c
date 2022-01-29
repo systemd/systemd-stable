042458baa5cd7ab6284b21f828612f0cf17190b1
Dmitry V. Levin <ldv@strace.io>
2022-01-29 20:00:00 +0000

    tmpfiles: avoid double specifier expansion in L lines
    Starting with commit 2f3b873a4973, when the path contains a specifier
    and the argument is omitted, tmpfiles used to perform specifier
    expansions twice: first specifier expansion was applied to the path
    itself, and afterwards the result of the first expansion was used
    in the constructed argument and expanded once again.
    
    Resolves: #26244
    Fixes: 2f3b873a4973 ("tmpfiles: copy/link /usr/share/factory/ files when the source argument is omitted")
    (cherry picked from commit 05381430a5d632ffc716fb4c89328952bd8ccafd)

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -3311,12 +3311,6 @@ static int parse_line(
                         *invalid_config = true;
                         return log_syntax(NULL, LOG_ERR, fname, line, SYNTHETIC_ERRNO(EBADMSG), "base64 decoding not supported for symlink targets.");
                 }
-
-                if (!i.argument) {
-                        i.argument = path_join("/usr/share/factory", i.path);
-                        if (!i.argument)
-                                return log_oom();
-                }
                 break;
 
         case WRITE_FILE:
@@ -3425,6 +3419,14 @@ static int parse_line(
         }
 
         switch (i.type) {
+        case CREATE_SYMLINK:
+                if (!i.argument) {
+                        i.argument = path_join("/usr/share/factory", i.path);
+                        if (!i.argument)
+                                return log_oom();
+                }
+                break;
+
         case COPY_FILES:
                 if (!i.argument) {
                         i.argument = path_join("/usr/share/factory", i.path);
