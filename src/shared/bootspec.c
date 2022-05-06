412b89a6e8055f2c8c9db4b6b847f081e00461ff
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-05-06 17:36:47 +0200

    shared/bootspec: avoid crashing on config without a value
    (cherry picked from commit b6bd2562ebb01b48cdb55a970d9daa1799b59876)

--- src/shared/bootspec.c
+++ src/shared/bootspec.c
@@ -124,6 +124,13 @@ static int boot_entry_load(
                         continue;
                 }
 
+                if (isempty(p)) {
+                        /* Some fields can reasonably have an empty value. In other cases warn. */
+                        if (!STR_IN_SET(field, "options", "devicetree-overlay"))
+                                log_warning("%s:%u: Field %s without value", tmp.path, line, field);
+                        continue;
+                }
+
                 if (streq(field, "title"))
                         r = free_and_strdup(&tmp.title, p);
                 else if (streq(field, "version"))
