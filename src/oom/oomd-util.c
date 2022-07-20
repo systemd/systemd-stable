1e29d934def95ec34241020bf5b5e68c02876561
Nick Rosbrook <nick.rosbrook@canonical.com>
2022-07-20 11:16:37 -0400

    oomd: fix off-by-one when dumping kill candidates
    When we kill a cgroup that is towards the end of the sorted candidate
    list (i.e. when we have to resort to killing a candidate with
    ManagedOOMPreference=avoid), this cgroup is not logged in the candidate
    list. This is due to an off-by-one error when assigning dump_until.
    
    (cherry picked from commit b814de305445f59baa289ab926cbe6ca05ac26a4)

--- src/oom/oomd-util.c
+++ src/oom/oomd-util.c
@@ -272,7 +272,7 @@ int oomd_kill_by_pgscan_rate(Hashmap *h, const char *prefix, bool dry_run, char 
                         continue; /* Try to find something else to kill */
                 }
 
-                dump_until = MAX(dump_until, i);
+                dump_until = MAX(dump_until, i + 1);
                 char *selected = strdup(sorted[i]->path);
                 if (!selected)
                         return -ENOMEM;
@@ -316,7 +316,7 @@ int oomd_kill_by_swap_usage(Hashmap *h, uint64_t threshold_usage, bool dry_run, 
                         continue; /* Try to find something else to kill */
                 }
 
-                dump_until = MAX(dump_until, i);
+                dump_until = MAX(dump_until, i + 1);
                 char *selected = strdup(sorted[i]->path);
                 if (!selected)
                         return -ENOMEM;
