systemd/systemd

generator: skip fsck if fsck command is missing
This is useful for systems which don't have any fsck.

We already skip emitting the fsck dependency when the fsck.$fstype helper
is missing, but fstab-generator doesn't necessarily know the fstype when
handling the root= parameter.

Previously, systemd-fsck was started for these mounts and then exited
immediately because it couldn't find the fsck.$fstype helper.

src/shared/generator.c
@@ -166,6 +166,15 @@ int generator_write_fsck_deps(
                 return 0;
         }
 
+        r = fsck_available();
+        if (r < 0)
+                log_warning_errno(r, "Checking was requested for %s, but couldn't detect if the fsck command may be used, proceeding: %m", what);
+        else if (r == 0) {
+                /* treat missing fsck as essentially OK */
+                log_debug("Checking was requested for %s, but the fsck command does not exist.", what);
+                return 0;
+        }
+
         if (!isempty(fstype) && !streq(fstype, "auto")) {
                 r = fsck_exists(fstype);
                 if (r < 0)

systemd/systemd-stable

(cherry picked from commit 13556724379a52951eb1977c2b7989a0159fd77c)
(cherry picked from commit 73db7d99323c236625656f906eb4e429613d324b)

src/shared/generator.c
@@ -164,12 +164,21 @@ int generator_write_fsck_deps(
         }
 
         if (!isempty(fstype) && !streq(fstype, "auto")) {
-                r = fsck_exists(fstype);
+                r = fsck_exists_for_fstype(fstype);
                 if (r < 0)
                         log_warning_errno(r, "Checking was requested for %s, but couldn't detect if fsck.%s may be used, proceeding: %m", what, fstype);
                 else if (r == 0) {
                         /* treat missing check as essentially OK */
                         log_debug("Checking was requested for %s, but fsck.%s does not exist.", what, fstype);
+                        return 0;
+                }
+        } else {
+                r = fsck_exists();
+                if (r < 0)
+                        log_warning_errno(r, "Checking was requested for %s, but couldn't detect if the fsck command may be used, proceeding: %m", what);
+                else if (r == 0) {
+                        /* treat missing fsck as essentially OK */
+                        log_debug("Checking was requested for %s, but the fsck command does not exist.", what);
                         return 0;
                 }
         }