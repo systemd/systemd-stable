systemd/systemd

core: ensure init.scope is realized after drop-ins have been loaded
If we add a drop-in for init.scope (e.g.: to set some memory limit),
it will be loaded long after the cgroup has already been realized.
Do it again at the end of the booting sequence.

test/TEST-55-OOMD/test.sh
@@ -17,6 +17,12 @@ test_append_files() {
         cat >>"${initdir:?}/etc/fstab" <<EOF
 UUID=$(blkid -o value -s UUID "${LOOPDEV}p2")    none    swap    defaults 0 0
 EOF
+
+        mkdir -p "${initdir:?}/lib/systemd/system/init.scope.d/"
+        cat >>"${initdir:?}/lib/systemd/system/init.scope.d/test-55-oomd.conf" <<EOF
+[Scope]
+MemoryHigh=1G
+EOF
     )
 }
 

systemd/systemd-stable

Do it again when creating the special unit.
(cherry picked from commit 020b2e41ea776cff73392da8084a0725b590d245)
(cherry picked from commit 786b7a7208cfb585b70659a8e3ac5180e85d0647)
(cherry picked from commit ffa329c45c945256f1ee397c1a5f56ce8dded412)
(cherry picked from commit 02332752b33289cc33f7db0222fc26feb47326cc)

test/TEST-55-OOMD/test.sh
@@ -16,6 +16,12 @@ test_append_files() {
         cat >>"${initdir:?}/etc/fstab" <<EOF
 UUID=$(blkid -o value -s UUID "${LOOPDEV}p2")    none    swap    defaults 0 0
 EOF
+
+        mkdir -p "${initdir:?}/etc/systemd/system/init.scope.d/"
+        cat >>"${initdir:?}/etc/systemd/system/init.scope.d/test-55-oomd.conf" <<EOF
+[Scope]
+MemoryHigh=10G
+EOF
     )
 }
 