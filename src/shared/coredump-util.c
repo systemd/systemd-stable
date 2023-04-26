760986639caa08f56ab7b3184018fbe42ff00ee4
Luca Boccassi <bluca@debian.org>
2023-04-26 14:18:04 +0100

    coredump filter: fix stack overflow with =all
    We translate 'all' to UNIT64_MAX, which has a lot more 'f's. Use the
    helper macro, since a decimal uint64_t will always be >> than a hex
    representation.
    
    root@image:~# systemd-run -t --property CoredumpFilter=all ls /tmp
    Running as unit: run-u13.service
    Press ^] three times within 1s to disconnect TTY.
    *** stack smashing detected ***: terminated
    [137256.320511] systemd[1]: run-u13.service: Main process exited, code=dumped, status=6/ABRT
    [137256.320850] systemd[1]: run-u13.service: Failed with result 'core-dump'.
    
    (cherry picked from commit 37232d55a7bcace37280e28b207c85f5ca9b3f6b)

--- src/shared/coredump-util.c
+++ src/shared/coredump-util.c
@@ -66,9 +66,9 @@ int coredump_filter_mask_from_string(const char *s, uint64_t *ret) {
 }
 
 int set_coredump_filter(uint64_t value) {
-        char t[STRLEN("0xFFFFFFFF")];
+        char t[HEXADECIMAL_STR_MAX(uint64_t)];
 
-        sprintf(t, "0x%"PRIx64, value);
+        xsprintf(t, "0x%"PRIx64, value);
 
         return write_string_file("/proc/self/coredump_filter", t,
                                  WRITE_STRING_FILE_VERIFY_ON_FAILURE|WRITE_STRING_FILE_DISABLE_BUFFER);
