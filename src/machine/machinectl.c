ab0a46f257e3ff8eefd189da23c9149682962e81
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-12 14:43:23 +0200

    machine: fix a memory leak when showing multiple images
    + machinectl image-status container1 container1 container0 container1 container2 container3 container4
     =================================================================
     ==1354==ERROR: LeakSanitizer: detected memory leaks
     Direct leak of 4704 byte(s) in 6 object(s) allocated from:
         #0 0x7fc3670ba097 in calloc (/lib64/libasan.so.8+0xba097)
         #1 0x7fc365e91e8e in message_from_header ../src/libsystemd/sd-bus/bus-message.c:372
         #2 0x7fc365e92dfd in bus_message_from_malloc ../src/libsystemd/sd-bus/bus-message.c:421
         #3 0x7fc365f089a8 in bus_socket_make_message ../src/libsystemd/sd-bus/bus-socket.c:1165
         #4 0x7fc365f0affe in bus_socket_read_message ../src/libsystemd/sd-bus/bus-socket.c:1294
         #5 0x7fc365f2db71 in bus_read_message ../src/libsystemd/sd-bus/sd-bus.c:2082
         #6 0x7fc365f33352 in sd_bus_call ../src/libsystemd/sd-bus/sd-bus.c:2483
         #7 0x7fc365e4da61 in sd_bus_call_methodv ../src/libsystemd/sd-bus/bus-convenience.c:183
         #8 0x7fc3658789e8 in bus_call_method ../src/shared/bus-locator.c:109
         #9 0x413b76 in show_image ../src/machine/machinectl.c:1014
         #10 0x7fc365c5c8cf in dispatch_verb ../src/shared/verbs.c:103
         #11 0x42e992 in machinectl_main ../src/machine/machinectl.c:2981
         #12 0x42ebbd in run ../src/machine/machinectl.c:3006
         #13 0x42ece3 in main ../src/machine/machinectl.c:3009
         #14 0x7fc36444a50f in __libc_start_call_main (/lib64/libc.so.6+0x2750f)
     Indirect leak of 666 byte(s) in 6 object(s) allocated from:
         #0 0x7fc3670b95b5 in __interceptor_realloc.part.0 (/lib64/libasan.so.8+0xb95b5)
         #1 0x7fc365f09822 in bus_socket_read_message ../src/libsystemd/sd-bus/bus-socket.c:1214
         #2 0x7fc365f2db71 in bus_read_message ../src/libsystemd/sd-bus/sd-bus.c:2082
         #3 0x7fc365f33352 in sd_bus_call ../src/libsystemd/sd-bus/sd-bus.c:2483
         #4 0x7fc365e4da61 in sd_bus_call_methodv ../src/libsystemd/sd-bus/bus-convenience.c:183
         #5 0x7fc3658789e8 in bus_call_method ../src/shared/bus-locator.c:109
         #6 0x413b76 in show_image ../src/machine/machinectl.c:1014
         #7 0x7fc365c5c8cf in dispatch_verb ../src/shared/verbs.c:103
         #8 0x42e992 in machinectl_main ../src/machine/machinectl.c:2981
         #9 0x42ebbd in run ../src/machine/machinectl.c:3006
         #10 0x42ece3 in main ../src/machine/machinectl.c:3009
         #11 0x7fc36444a50f in __libc_start_call_main (/lib64/libc.so.6+0x2750f)
     Indirect leak of 12 byte(s) in 6 object(s) allocated from:
         #0 0x7fc36707243b in strdup (/lib64/libasan.so.8+0x7243b)
         #1 0x7fc365ec1543 in message_parse_fields ../src/libsystemd/sd-bus/bus-message.c:4125
         #2 0x7fc365e93586 in bus_message_from_malloc ../src/libsystemd/sd-bus/bus-message.c:443
         #3 0x7fc365f089a8 in bus_socket_make_message ../src/libsystemd/sd-bus/bus-socket.c:1165
         #4 0x7fc365f0affe in bus_socket_read_message ../src/libsystemd/sd-bus/bus-socket.c:1294
         #5 0x7fc365f2db71 in bus_read_message ../src/libsystemd/sd-bus/sd-bus.c:2082
         #6 0x7fc365f33352 in sd_bus_call ../src/libsystemd/sd-bus/sd-bus.c:2483
         #7 0x7fc365e4da61 in sd_bus_call_methodv ../src/libsystemd/sd-bus/bus-convenience.c:183
         #8 0x7fc3658789e8 in bus_call_method ../src/shared/bus-locator.c:109
         #9 0x413b76 in show_image ../src/machine/machinectl.c:1014
         #10 0x7fc365c5c8cf in dispatch_verb ../src/shared/verbs.c:103
         #11 0x42e992 in machinectl_main ../src/machine/machinectl.c:2981
         #12 0x42ebbd in run ../src/machine/machinectl.c:3006
         #13 0x42ece3 in main ../src/machine/machinectl.c:3009
         #14 0x7fc36444a50f in __libc_start_call_main (/lib64/libc.so.6+0x2750f)
     SUMMARY: AddressSanitizer: 5382 byte(s) leaked in 18 allocation(s).
    
    (cherry picked from commit 4b6ce580eee3f70412637c1df4239e448995535f)
    (cherry picked from commit e6a719598c80c284fb4b570c0eb89e7416616a98)
    (cherry picked from commit 8dd68c29ff5a56bb4215fe9fcbb7399e21f6560c)

--- src/machine/machinectl.c
+++ src/machine/machinectl.c
@@ -993,9 +993,7 @@ static int show_image_properties(sd_bus *bus, const char *path, bool *new_line) 
 }
 
 static int show_image(int argc, char *argv[], void *userdata) {
-
         _cleanup_(sd_bus_error_free) sd_bus_error error = SD_BUS_ERROR_NULL;
-        _cleanup_(sd_bus_message_unrefp) sd_bus_message *reply = NULL;
         bool properties, new_line = false;
         sd_bus *bus = userdata;
         int r = 0;
@@ -1020,6 +1018,7 @@ static int show_image(int argc, char *argv[], void *userdata) {
         }
 
         for (int i = 1; i < argc; i++) {
+                _cleanup_(sd_bus_message_unrefp) sd_bus_message *reply = NULL;
                 const char *path = NULL;
 
                 r = bus_call_method(bus, bus_machine_mgr, "GetImage", &error, &reply, "s", argv[i]);
