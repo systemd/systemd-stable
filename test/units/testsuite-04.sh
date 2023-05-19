c31f42507d5a5045471a6e4158b18cd4bb9a4fbb
Frantisek Sumsal <frantisek@sumsal.cz>
2023-05-19 17:37:17 +0200

    test: check if we can use --merge with --follow
    Provides coverage for #24565.
    
    (cherry picked from commit c11c50a53f677f3977f3144b9fc5f02a3516de9f)

--- test/units/testsuite-04.sh
+++ test/units/testsuite-04.sh
@@ -178,8 +178,12 @@ sleep 3
 [[ ! -f "/i-lose-my-logs" ]]
 systemctl stop forever-print-hola
 
+set +o pipefail
 # https://github.com/systemd/systemd/issues/15528
-journalctl --follow --file=/var/log/journal/*/* | head -n1 || [[ $? -eq 1 ]]
+journalctl --follow --file=/var/log/journal/*/* | head -n1 | grep .
+# https://github.com/systemd/systemd/issues/24565
+journalctl --follow --merge | head -n1 | grep .
+set -o pipefail
 
 function add_logs_filtering_override() {
     UNIT=${1:?}
