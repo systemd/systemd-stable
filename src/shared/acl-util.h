0789a560d767e9799464223f01787e12e9e5e6a0
Lennart Poettering <lennart@poettering.net>
2023-07-04 22:26:52 +0200

    execute: fix credential dir handling for fs which support ACLs
    When the credential dir is backed by an fs that supports ACLs we must be
    more careful with adjusting the 'x' bit of the directory, as any chmod()
    call on the dir will reset the mask entry of the ACL entirely which we
    don't want. Hence, do a manual set of ACL changes, that only add/drop
    the 'x' bit but otherwise leave the ACL as it is.
    
    This matters if we use tmpfs rather than ramfs to store credentials.
    
    (cherry picked from commit f76ce81b91db1dac0d0a012e1cc903639002dd0a)
    (cherry picked from commit ee3ed28f42605402873ca2169cfb4e6f3cbfbdf9)
    (cherry picked from commit ef943b27cfa28e065aa642037e74903e610f265e)

--- src/shared/acl-util.h
+++ src/shared/acl-util.h
@@ -4,6 +4,9 @@
 #include <errno.h>
 #include <unistd.h>
 
+int fd_acl_make_read_only_fallback(int fd);
+int fd_acl_make_writable_fallback(int fd);
+
 #if HAVE_ACL
 #include <acl/libacl.h>
 #include <stdbool.h>
@@ -18,6 +21,9 @@ int acl_search_groups(const char* path, char ***ret_groups);
 int parse_acl(const char *text, acl_t *acl_access, acl_t *acl_default, bool want_mask);
 int acls_for_file(const char *path, acl_type_t type, acl_t new, acl_t *acl);
 int fd_add_uid_acl_permission(int fd, uid_t uid, unsigned mask);
+
+int fd_acl_make_read_only(int fd);
+int fd_acl_make_writable(int fd);
 
 /* acl_free takes multiple argument types.
  * Multiple cleanup functions are necessary. */
@@ -37,4 +43,13 @@ DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(gid_t*, acl_free_gid_tp, NULL);
 static inline int fd_add_uid_acl_permission(int fd, uid_t uid, unsigned mask) {
         return -EOPNOTSUPP;
 }
+
+static inline int fd_acl_make_read_only(int fd) {
+        return fd_acl_make_read_only_fallback(fd);
+}
+
+static inline int fd_acl_make_writable(int fd) {
+        return fd_acl_make_writable_fallback(fd);
+}
+
 #endif
