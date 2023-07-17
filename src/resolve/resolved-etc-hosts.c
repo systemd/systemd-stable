systemd/systemd

resolved: fix the canonical name returned by hosts lookup by address
In etc_hosts_lookup_by_address(), make sure the canonical name of the given
address is returned first in the list of names that address resolves to.

Resolves: #25088

src/resolve/resolved-etc-hosts.c
@@ -381,6 +381,21 @@ static int manager_etc_hosts_read(Manager *m) {
         return 1;
 }
 
+static int answer_add_ptr(DnsAnswer *answer, DnsResourceKey *key, const char *name) {
+
+        _cleanup_(dns_resource_record_unrefp) DnsResourceRecord *rr = NULL;
+
+        rr = dns_resource_record_new(key);
+        if (!rr)
+                return -ENOMEM;
+
+        rr->ptr.name = strdup(name);
+        if (!rr->ptr.name)
+                return -ENOMEM;
+
+        return dns_answer_add(answer, rr, 0, DNS_ANSWER_AUTHENTICATED, NULL);
+}
+
 static int etc_hosts_lookup_by_address(
                 EtcHosts *hosts,
                 DnsQuestion *q,
@@ -427,18 +442,17 @@ static int etc_hosts_lookup_by_address(
                 if (r < 0)
                         return r;
 
+                if (item->canon_name) {
+                        r = answer_add_ptr(*answer, found_ptr, item->canon_name);
+                        if (r < 0)
+                                return r;
+                }
+
                 SET_FOREACH(n, item->names) {
-                        _cleanup_(dns_resource_record_unrefp) DnsResourceRecord *rr = NULL;
+                        if (n == item->canon_name)
+                                continue;
 
-                        rr = dns_resource_record_new(found_ptr);
-                        if (!rr)
-                                return -ENOMEM;
-
-                        rr->ptr.name = strdup(n);
-                        if (!rr->ptr.name)
-                                return -ENOMEM;
-
-                        r = dns_answer_add(*answer, rr, 0, DNS_ANSWER_AUTHENTICATED, NULL);
+                        r = answer_add_ptr(*answer, found_ptr, n);
                         if (r < 0)
                                 return r;
                 }

systemd/systemd-stable

(cherry picked from commit 0ff8f2a33a8f7c225860388faf43fa83f106cfe3)

src/resolve/resolved-etc-hosts.c
@@ -380,6 +380,20 @@ static int manager_etc_hosts_read(Manager *m) {
         return 1;
 }
 
+static int answer_add_ptr(DnsAnswer *answer, DnsResourceKey *key, const char *name) {
+        _cleanup_(dns_resource_record_unrefp) DnsResourceRecord *rr = NULL;
+
+        rr = dns_resource_record_new(key);
+        if (!rr)
+                return -ENOMEM;
+
+        rr->ptr.name = strdup(name);
+        if (!rr->ptr.name)
+                return -ENOMEM;
+
+        return dns_answer_add(answer, rr, 0, DNS_ANSWER_AUTHENTICATED, NULL);
+}
+
 static int etc_hosts_lookup_by_address(
                 EtcHosts *hosts,
                 DnsQuestion *q,
@@ -426,18 +440,17 @@ static int etc_hosts_lookup_by_address(
                 if (r < 0)
                         return r;
 
+                if (item->canonical_name) {
+                        r = answer_add_ptr(*answer, found_ptr, item->canonical_name);
+                        if (r < 0)
+                                return r;
+                }
+
                 SET_FOREACH(n, item->names) {
-                        _cleanup_(dns_resource_record_unrefp) DnsResourceRecord *rr = NULL;
+                        if (n == item->canonical_name)
+                                continue;
 
-                        rr = dns_resource_record_new(found_ptr);
-                        if (!rr)
-                                return -ENOMEM;
-
-                        rr->ptr.name = strdup(n);
-                        if (!rr->ptr.name)
-                                return -ENOMEM;
-
-                        r = dns_answer_add(*answer, rr, 0, DNS_ANSWER_AUTHENTICATED, NULL);
+                        r = answer_add_ptr(*answer, found_ptr, n);
                         if (r < 0)
                                 return r;
                 }