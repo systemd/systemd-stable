beefda22dc05f79c7c4a31a40ae98ab96fe103eb
Lennart Poettering <lennart@poettering.net>
2023-11-02 22:32:36 +0100

    cryptsetup: pass AskPasswordFlags down into pkcs11 module
    The pkcs11 cryptsetup token module is a bit different from the tpm2 +
    fido2 ones: it asks for the PIN itself, rather than bubbling up a
    request to get a PIN. That's because it might need multiple, and because
    we don't want to destroy a the pkcs11 session half-way and thus risk
    increasing pin counters.
    
    Hence, we sometimes ask for PINs from our code, rather than let the
    libcryptsetup caller do that. So far we didn't pass the AskPasswordFlags
    field down into the module though. Fix that.
    
    Fixes: #28665
    (cherry picked from commit a758a128709cbcd99eddd1de6ea0e9846b9ce2eb)
    (cherry picked from commit d1e542ca9c2124fe6fa1f49f3d23b4d0336a442e)
    (cherry picked from commit fb610013416ebd1e99184cc3b68985e90be4713f)

--- src/shared/pkcs11-util.c
+++ src/shared/pkcs11-util.c
@@ -234,6 +234,7 @@ int pkcs11_token_login(
                 const char *key_name,
                 const char *credential_name,
                 usec_t until,
+                AskPasswordFlags ask_password_flags,
                 bool headless,
                 char **ret_used_pin) {
 
@@ -310,7 +311,7 @@ int pkcs11_token_login(
                                 return log_oom();
 
                         /* We never cache PINs, simply because it's fatal if we use wrong PINs, since usually there are only 3 tries */
-                        r = ask_password_auto(text, icon_name, id, key_name, credential_name, until, 0, &passwords);
+                        r = ask_password_auto(text, icon_name, id, key_name, credential_name, until, ask_password_flags, &passwords);
                         if (r < 0)
                                 return log_error_errno(r, "Failed to query PIN for security token '%s': %m", token_label);
                 }
@@ -961,6 +962,8 @@ struct pkcs11_acquire_certificate_callback_data {
         char *pin_used;
         X509 *cert;
         const char *askpw_friendly_name, *askpw_icon_name;
+        AskPasswordFlags askpw_flags;
+        bool headless;
 };
 
 static void pkcs11_acquire_certificate_callback_data_release(struct pkcs11_acquire_certificate_callback_data *data) {
@@ -989,7 +992,19 @@ static int pkcs11_acquire_certificate_callback(
 
         /* Called for every token matching our URI */
 
-        r = pkcs11_token_login(m, session, slot_id, token_info, data->askpw_friendly_name, data->askpw_icon_name, "pkcs11-pin", "pkcs11-pin", UINT64_MAX, false, &pin_used);
+        r = pkcs11_token_login(
+                        m,
+                        session,
+                        slot_id,
+                        token_info,
+                        data->askpw_friendly_name,
+                        data->askpw_icon_name,
+                        "pkcs11-pin",
+                        "pkcs11-pin",
+                        UINT64_MAX,
+                        data->askpw_flags,
+                        data->headless,
+                        &pin_used);
         if (r < 0)
                 return r;
 
@@ -1220,6 +1235,7 @@ int pkcs11_crypt_device_callback(
                         "pkcs11-pin",
                         "cryptsetup.pkcs11-pin",
                         data->until,
+                        data->askpw_flags,
                         data->headless,
                         NULL);
         if (r < 0)
