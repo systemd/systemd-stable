df39397eb650c89dbcbaa8636f891ac7e04ea673
Mike Yuan <me@yhndnzj.com>
2023-11-30 19:16:49 +0800

    core/exec-invoke: rename flags_fds to flag_fds
    (cherry picked from commit d8da25b5d9c9251baf64cff1c8612a465d7b5415)
    (cherry picked from commit 3db2c1a90b746aa778a45aaa363f117853ab0ac5)
    (cherry picked from commit 44c616276b0b54e238dbe92b89dc3a544a3ff8d2)
    (cherry picked from commit 63528755ef3e9c59f5e6a7332df0fbb0d54cf34b)

--- src/core/execute.c
+++ src/core/execute.c
@@ -151,7 +151,7 @@ static int shift_fds(int fds[], size_t n_fds) {
         return 0;
 }
 
-static int flags_fds(const int fds[], size_t n_socket_fds, size_t n_storage_fds, bool nonblock) {
+static int flag_fds(const int fds[], size_t n_socket_fds, size_t n_storage_fds, bool nonblock) {
         size_t n_fds;
         int r;
 
@@ -160,6 +160,7 @@ static int flags_fds(const int fds[], size_t n_socket_fds, size_t n_storage_fds,
                 return 0;
 
         assert(fds);
+        assert(fds || n_fds == 0);
 
         /* Drops/Sets O_NONBLOCK and FD_CLOEXEC from the file flags.
          * O_NONBLOCK only applies to socket activation though. */
@@ -4855,7 +4856,7 @@ static int exec_child(
         if (r >= 0)
                 r = shift_fds(fds, n_fds);
         if (r >= 0)
-                r = flags_fds(fds, n_socket_fds, n_storage_fds, context->non_blocking);
+                r = flag_fds(fds, n_socket_fds, n_storage_fds, context->non_blocking);
         if (r < 0) {
                 *exit_status = EXIT_FDS;
                 return log_unit_error_errno(unit, r, "Failed to adjust passed file descriptors: %m");
