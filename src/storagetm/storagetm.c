adbbb8b3135fec442fb1f8a93b17f48d0729c8bb
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-01-03 05:07:40 +0900

    storagetm: always hash stat.st_mode
    To make the hash function consistent with the compare function.
    
    (cherry picked from commit 30c1cded77b8b0456c8793a7b3ce0ffe00a798ff)

--- src/storagetm/storagetm.c
+++ src/storagetm/storagetm.c
@@ -788,9 +788,10 @@ typedef struct Context {
 static void device_hash_func(const struct stat *q, struct siphash *state) {
         assert(q);
 
+        mode_t m = q->st_mode & S_IFMT;
+        siphash24_compress(&m, sizeof(m), state);
+
         if (S_ISBLK(q->st_mode) || S_ISCHR(q->st_mode)) {
-                mode_t m = q->st_mode & S_IFMT;
-                siphash24_compress(&m, sizeof(m), state);
                 siphash24_compress(&q->st_rdev, sizeof(q->st_rdev), state);
                 return;
         }
