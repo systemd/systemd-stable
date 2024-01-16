722bed1dab87306866a385120c1787cd743d26cc
Frantisek Sumsal <frantisek@sumsal.cz>
2024-01-16 22:42:39 +0100

    Reorder arguments for calloc()-like functions, part #2
    To appease gcc-14's -Wcalloc-transposed-args check.
    
    Follow-up for 2a9ab0974bb290bc66dc84d909c33d23361b0752.
    
    (cherry picked from commit fdd84270df0062fad68783eea8b51a6ed87b67cd)
    (cherry picked from commit b24c90d567778ee13893f5edfa666042df76e4c0)
    (cherry picked from commit 163d7829186d82c92ded46f41346432f74d2deb2)
    (cherry picked from commit 66ce5973d2fb855a1a3a27fa308f7aeedb767264)

--- src/boot/efi/util.h
+++ src/boot/efi/util.h
@@ -51,7 +51,7 @@ static inline void *xmalloc(size_t size) {
 }
 
 _malloc_ _alloc_(1, 2) _returns_nonnull_ _warn_unused_result_
-static inline void *xmalloc_multiply(size_t size, size_t n) {
+static inline void *xmalloc_multiply(size_t n, size_t size) {
         assert_se(!__builtin_mul_overflow(size, n, &size));
         return xmalloc(size);
 }
@@ -68,7 +68,7 @@ static inline void *xrealloc(void *p, size_t old_size, size_t new_size) {
 }
 
 #define xpool_print(fmt, ...) ((char16_t *) ASSERT_SE_PTR(PoolPrint((fmt), ##__VA_ARGS__)))
-#define xnew(type, n) ((type *) xmalloc_multiply(sizeof(type), (n)))
+#define xnew(type, n) ((type *) xmalloc_multiply((n), sizeof(type)))
 
 typedef struct {
         EFI_PHYSICAL_ADDRESS addr;
