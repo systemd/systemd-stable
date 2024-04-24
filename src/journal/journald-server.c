acb8571465adae1f8d40b10f5319f4b8cc5ae3b0
Mike Yuan <me@yhndnzj.com>
2024-04-25 00:55:31 +0800

    journald-server: drop spuriously doubled '=' for OBJECT_SYSTEMD_INVOCATION_ID
    (cherry picked from commit 6cb8286aa3f3cca6cc565942abe8597f35c11a6c)
    (cherry picked from commit 9fd78d3052c681d48ce04f13243b962c713d00b3)
    (cherry picked from commit 9e8116e77a8a2e97f28cf0fa1b99a99915760b11)
    (cherry picked from commit 694dc15d393c1b3e268b005461d6dd5379581dc4)

--- src/journal/journald-server.c
+++ src/journal/journald-server.c
@@ -1016,7 +1016,7 @@ static void dispatch_message_real(
                 IOVEC_ADD_STRING_FIELD(iovec, n, o->slice, "OBJECT_SYSTEMD_SLICE");
                 IOVEC_ADD_STRING_FIELD(iovec, n, o->user_slice, "OBJECT_SYSTEMD_USER_SLICE");
 
-                IOVEC_ADD_ID128_FIELD(iovec, n, o->invocation_id, "OBJECT_SYSTEMD_INVOCATION_ID=");
+                IOVEC_ADD_ID128_FIELD(iovec, n, o->invocation_id, "OBJECT_SYSTEMD_INVOCATION_ID");
         }
 
         assert(n <= m);
