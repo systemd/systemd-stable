262eb407a493e4c1455596c8906e1305e2bcbbe1
Yu Watanabe <watanabe.yu+github@gmail.com>
2024-05-01 03:31:25 +0900

    sd-journal: verify monotonic timestamp before assigning result
    Previously, ret_boot_id was assigned even when the function failed due
    to an invalid monotonic timestamp stored for a journal entry.
    
    (cherry picked from commit c9df4714286223017aff1b2f32f96058d249d8ab)
    (cherry picked from commit 6549d31b2c88ffecae9502aff6ff5e8fd4414bb6)

--- src/libsystemd/sd-journal/sd-journal.c
+++ src/libsystemd/sd-journal/sd-journal.c
@@ -2526,9 +2526,7 @@ _public_ int sd_journal_get_monotonic_usec(sd_journal *j, uint64_t *ret, sd_id12
         if (r < 0)
                 return r;
 
-        if (ret_boot_id)
-                *ret_boot_id = o->entry.boot_id;
-        else {
+        if (!ret_boot_id) {
                 sd_id128_t id;
 
                 r = sd_id128_get_boot(&id);
@@ -2545,6 +2543,8 @@ _public_ int sd_journal_get_monotonic_usec(sd_journal *j, uint64_t *ret, sd_id12
 
         if (ret)
                 *ret = t;
+        if (ret_boot_id)
+                *ret_boot_id = o->entry.boot_id;
 
         return 0;
 }
