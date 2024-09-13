104b8e725bd6dc223455f0f64447dedf0cc2afaf
Michal Koutný <mkoutny@suse.com>
2024-09-13 19:27:13 +0200

    core/cgroup: Apply IODevice*= directives in configured order
    Different device paths may resolve to same device node
    (lookup_block_device()), e.g.
            IOReadBandwidthMax=/dev/sda1 18879
            IOReadBandwidthMax=/dev/sda2 18878
    where both partitions resolve to /dev/sda and when these values are
    applied (they are associated with original paths, i.e. as if applied for
    different device) in the order from io_device_limits.
    
    The parsing code prepends, so they end up in reverse order wrt config
    file. Switch the direction so that the order of application matches the
    order of configuration -- i.e. semantics in all other unit file
    directives.
    
    Apply same change to all directives that use per-device lists. (The
    question whether partitions should be resolved to base device is
    independent.)
    
    And apply the changes equally to DBus properties write handlers.
    
    Fixes #34126
    
    (cherry picked from commit 0fa0dfa04465651a18107d503f9967f84bd761d1)
    (cherry picked from commit 00dfa7964b5e48a37596207ad8b2862b157cffaf)
    (cherry picked from commit f45acd05bec88521bb2f25bbd6c3792a35ad3a87)
    (cherry picked from commit 8846e2146a1ecb33f728aeec94867e9c85d51b55)
    (cherry picked from commit 52ccb2d5581587f4048b0f83df41941df7cdcb19)

--- src/core/load-fragment.c
+++ src/core/load-fragment.c
@@ -4237,7 +4237,7 @@ int config_parse_io_device_weight(
         w->path = TAKE_PTR(resolved);
         w->weight = u;
 
-        LIST_PREPEND(device_weights, c->io_device_weights, w);
+        LIST_APPEND(device_weights, c->io_device_weights, w);
         return 0;
 }
 
@@ -4308,7 +4308,7 @@ int config_parse_io_device_latency(
         l->path = TAKE_PTR(resolved);
         l->target_usec = usec;
 
-        LIST_PREPEND(device_latencies, c->io_device_latencies, l);
+        LIST_APPEND(device_latencies, c->io_device_latencies, l);
         return 0;
 }
 
@@ -4396,7 +4396,7 @@ int config_parse_io_limit(
                 for (ttype = 0; ttype < _CGROUP_IO_LIMIT_TYPE_MAX; ttype++)
                         l->limits[ttype] = cgroup_io_limit_defaults[ttype];
 
-                LIST_PREPEND(device_limits, c->io_device_limits, l);
+                LIST_APPEND(device_limits, c->io_device_limits, l);
         }
 
         l->limits[type] = num;
@@ -4477,7 +4477,7 @@ int config_parse_blockio_device_weight(
         w->path = TAKE_PTR(resolved);
         w->weight = u;
 
-        LIST_PREPEND(device_weights, c->blockio_device_weights, w);
+        LIST_APPEND(device_weights, c->blockio_device_weights, w);
         return 0;
 }
 
@@ -4564,7 +4564,7 @@ int config_parse_blockio_bandwidth(
                 b->rbps = CGROUP_LIMIT_MAX;
                 b->wbps = CGROUP_LIMIT_MAX;
 
-                LIST_PREPEND(device_bandwidths, c->blockio_device_bandwidths, b);
+                LIST_APPEND(device_bandwidths, c->blockio_device_bandwidths, b);
         }
 
         if (read)
