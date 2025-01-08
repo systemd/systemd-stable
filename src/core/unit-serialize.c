systemd/systemd

core/unit-serialize: fix serialization of markers

src/core/unit-serialize.c
@@ -21,10 +21,12 @@ static int serialize_markers(FILE *f, unsigned markers) {
         if (markers == 0)
                 return 0;
 
+        bool space = false;
+
         fputs("markers=", f);
         for (UnitMarker m = 0; m < _UNIT_MARKER_MAX; m++)
                 if (FLAGS_SET(markers, 1u << m))
-                        fputs(unit_marker_to_string(m), f);
+                        fputs_with_separator(f, unit_marker_to_string(m), /* separator = */ NULL, &space);
         fputc('\n', f);
         return 0;
 }

systemd/systemd-stable

Follow-up for ff68472a20c208121b69ea13586f3105a219bc14
(cherry picked from commit 5ce8d7d83902e920a34488c4193d9bc4b5bb70ae)
(cherry picked from commit 6f30e56bf3e5272e83cff11d153ae70c3ffb7624)
(cherry picked from commit 4a9f42e19782a9d6ab8b444511fa2a319ea1a804)
(cherry picked from commit b030bfba78d06f90759226eadfded84f0543ddcf)
(cherry picked from commit bf01f3d692b6ad779c9bec2495f57c56864d8aa1)

src/core/unit-serialize.c
@@ -39,10 +39,12 @@ static int serialize_markers(FILE *f, unsigned markers) {
         if (markers == 0)
                 return 0;
 
+        bool space = false;
+
         fputs("markers=", f);
         for (UnitMarker m = 0; m < _UNIT_MARKER_MAX; m++)
                 if (FLAGS_SET(markers, 1u << m))
-                        fputs(unit_marker_to_string(m), f);
+                        fputs_with_space(f, unit_marker_to_string(m), /* separator = */ NULL, &space);
         fputc('\n', f);
         return 0;
 }