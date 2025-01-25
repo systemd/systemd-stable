58f00846b996588c28fcf04d21af98f8f88cf12c
Daan De Meyer <daan.j.demeyer@gmail.com>
2025-01-25 20:42:54 +0100

    boot: Improve log message
    (cherry picked from commit ff83795469a20af02a9bf3285992128799b16302)
    (cherry picked from commit 05a135c36e164bbda708af99597742788ef4eeea)
    (cherry picked from commit c1092e032f2c814beb559da00690a9f457416426)
    (cherry picked from commit 8d87145808ef7f84292b64bbb543fae4c748e57e)
    (cherry picked from commit e512aacbcfdf8e93044b73eeb328e27653e9c69b)

--- src/boot/efi/boot.c
+++ src/boot/efi/boot.c
@@ -1383,7 +1383,7 @@ static EFI_STATUS config_entry_bump_counters(ConfigEntry *entry) {
 
         err = root->Open(root, &handle, old_path, EFI_FILE_MODE_READ|EFI_FILE_MODE_WRITE, 0ULL);
         if (err != EFI_SUCCESS)
-                return log_error_status(err, "Error opening boot entry: %m");
+                return log_error_status(err, "Error opening boot entry '%ls': %m", old_path);
 
         err = get_file_info_harder(handle, &file_info, &file_info_size);
         if (err != EFI_SUCCESS)
