e56c478c80fb8399f2b7092df3b67b293c240152
Luca Boccassi <luca.boccassi@gmail.com>
2025-02-23 19:05:24 +0000

    repart: when using erofs and log level is not debug, use --quiet
    mkfs.erofs is extremely verbose and will log every single file added
    to the filesystem, which is a lot of them when building a rootfs.
    
    (cherry picked from commit 323213cfea8b78d44f63b8a83f74fbd1f79f1775)
    (cherry picked from commit b92ea51d000e69d032b0a8cf80bf2714a95e69c2)
    (cherry picked from commit 37ba21ae354ecccbc69a8b43c2620b3f80d32d66)

--- src/partition/repart.c
+++ src/partition/repart.c
@@ -4901,7 +4901,8 @@ static int context_mkfs(Context *context) {
                                                p->format);
 
                 r = make_filesystem(partition_target_path(t), p->format, strempty(p->new_label), root,
-                                    p->fs_uuid, arg_discard, /* quiet = */ false,
+                                    p->fs_uuid, arg_discard,
+                                    /* quiet = */ streq(p->format, "erofs") && !DEBUG_LOGGING,
                                     context->fs_sector_size, extra_mkfs_options);
                 if (r < 0)
                         return r;
@@ -6269,7 +6270,8 @@ static int context_minimize(Context *context) {
                                     strempty(p->new_label),
                                     root,
                                     fs_uuid,
-                                    arg_discard, /* quiet = */ false,
+                                    arg_discard,
+                                    /* quiet = */ streq(p->format, "erofs") && !DEBUG_LOGGING,
                                     context->fs_sector_size,
                                     extra_mkfs_options);
                 if (r < 0)
@@ -6350,7 +6352,7 @@ static int context_minimize(Context *context) {
                                     root,
                                     p->fs_uuid,
                                     arg_discard,
-                                    /* quiet = */ false,
+                                    /* quiet = */ streq(p->format, "erofs") && !DEBUG_LOGGING,
                                     context->fs_sector_size,
                                     extra_mkfs_options);
                 if (r < 0)
