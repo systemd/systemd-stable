34898e9c5c5d344c5214dfce72bf35e72770b20f
Mike Yuan <me@yhndnzj.com>
2025-03-05 18:35:50 +0100

    core/main: correct retval for reexec/switch-root/soft-reboot
    For these objectives we ought to execve() at the end, i.e. if we
    ever hit the return path something went wrong in do_reexecute().
    Let's properly report that via retval.
    
    (cherry picked from commit 590e0e3bacb012bc86ff46f8dbe3f1b415e9cafd)
    (cherry picked from commit efcd9c6e62dbc66e5bc70046e8ad1df2ad97947f)
    (cherry picked from commit 1282484941ca8e65de1586f15fcb09ad897f20e0)

--- src/core/main.c
+++ src/core/main.c
@@ -2031,7 +2031,7 @@ static int invoke_main_loop(
 
                         log_notice("Reexecuting.");
 
-                        *ret_retval = EXIT_SUCCESS;
+                        *ret_retval = EXIT_FAILURE;
                         *ret_switch_root_dir = *ret_switch_root_init = NULL;
 
                         return objective;
@@ -2054,7 +2054,7 @@ static int invoke_main_loop(
 
                         log_notice("Switching root.");
 
-                        *ret_retval = EXIT_SUCCESS;
+                        *ret_retval = EXIT_FAILURE;
 
                         /* Steal the switch root parameters */
                         *ret_switch_root_dir = TAKE_PTR(m->switch_root);
@@ -2074,7 +2074,7 @@ static int invoke_main_loop(
 
                         log_notice("Soft-rebooting.");
 
-                        *ret_retval = EXIT_SUCCESS;
+                        *ret_retval = EXIT_FAILURE;
                         *ret_switch_root_dir = TAKE_PTR(m->switch_root);
                         *ret_switch_root_init = NULL;
 
